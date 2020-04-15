74991608f7d23d88204c23039ebc499cdfeb750c
Lucas <malamanlucas@gmail.com>
2020-04-14 23:43:22 -0300

    x

--- restore.sh
+++ restore.sh
@@ -1,2 +1,2 @@
-psql -h localhost -d postgres -U postgres -p 5432 -q -f reset_db.sql &&
-psql -h localhost -d postgres -U postgres -p 5432 -q -f src/main/resources/nova_arquitera.sql
+PGPASSWORD=postgres psql -h localhost -d postgres -U postgres -p 5432 -q -f reset_db.sql &&
+PGPASSWORD=postgres psql -h localhost -d postgres -U postgres -p 5432 -q -f src/main/resources/nova_arquitera.sql
