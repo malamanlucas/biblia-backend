1ec0f29b8a24922995f02a84ef436a6a670f7782
Lucas Malaman <lucas.loliveira@mercadolivre.com>
2021-05-15 21:00:11 -0300

    adjust script

--- restore.sh
+++ restore.sh
@@ -1,5 +1,5 @@
 rm -f backup_biblia-backend-postgresql.sql &&
-  wget https://www.dropbox.com/s/cutkzbftr365mlr/backup_biblia-backend-postgresql.sql &&
+  wget https://www.dropbox.com/s/eifgqwjx7ifd9ej/backup_biblia-backend-postgresql.sql &&
 PGPASSWORD=postgres psql -h localhost -d postgres -U postgres -p 5432 -q -f reset_db.sql &&
   PGPASSWORD=postgres psql -h localhost -d postgres -U postgres -p 5432 -q -f backup_biblia-backend-postgresql.sql &&
     PGPASSWORD=postgres psql -h localhost -d postgres -U postgres -p 5432 -q -f create-function-unaccent.sql
